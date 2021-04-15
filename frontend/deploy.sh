23580888f6d056c5957e8d762f0950151fb8a98e
Cuti27 <jc3248427@gmail.com>
2021-04-15 13:47:37 +0200

    Añadido script para publicar en Muestra-TFG la parte visual

---
+++ frontend/deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m "deploy"
+git branch -M main
+git remote add origin https://github.com/Cuti27/Muestra-TFG.git
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f origin main
+
+cd -
